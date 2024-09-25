40be8e26496cb020587a8693ff4ed32f109bbd55
Nicola Quaia <nicolaquaia@gmail.com>
2024-09-25 10:23:03 +0200

    correct input

--- assignment_01/hawc_files/our_design/make_htc_files.py
+++ assignment_01/hawc_files/our_design/make_htc_files.py
@@ -15,7 +15,8 @@ if __name__ == '__main__':
                     append='_hawc2s_1wsp',
                     opt_path='./data/Group1_redesign_1wsp.opt',
                     compute_steady_states=True,
-                    save_power=True)
+                    save_power=True,
+                    save_induction=True)
 
     htc = MyHTC(ORIG_PATH)
     # make rigid hawc2s file for multi-wsp opt file
@@ -24,19 +25,21 @@ if __name__ == '__main__':
                     append='_hawc2s_multiwsp',
                     opt_path='./data/Group1_redesign_multiwsp.opt',
                     compute_steady_states=True,
-                    save_power=True)
-
+                    save_power=True,
+                    save_induction=True)
+    
     htc = MyHTC(ORIG_PATH)
     # make rigid hawc2s file for compute rigid opt file
     htc.make_hawc2s(SAVE_HAWC2S_DIR,
                     rigid=True,
                     append='_hawc2s_compute_rigid_opt',
                     opt_path='./data/dtu_10mw_rigid.opt',
-                    compute_steady_states=True,
-                    save_power=True,
-                    minpitch = 0.001070,
-                    opt_lambda=7.263157)
+                    compute_steady_states=False,
+                    save_power=False,
+                    compute_optimal_pitch_angle = True,
+                    minpitch = 0.000000,
+                    opt_lambda=7.263157,
+                    genspeed= (0, 50*8.337868262998404))
     
-
     # INSERT CODE HERE WHEN PROMPTED 
 
