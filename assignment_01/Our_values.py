d56e566a38de5834e33b165331047466194fa8ce
Nicola Quaia <nicolaquaia@gmail.com>
2024-09-27 10:50:40 +0200

    shinanigans

--- assignment_01/Our_values.py
+++ assignment_01/Our_values.py
@@ -2,8 +2,14 @@ import numpy as np
 
 #Our values
 R_X=89.16
+r_hub = 2.8
+B = 3 
+
 V_rated_Y = 11.122788574520852
 R_Y = 92.52423873190985
-TSR_opt = 7.2631578947368425
 
+
+TSR_opt = 7.2631578947368425
 omega_max = TSR_opt * V_rated_Y / R_Y * 60 / (2*np.pi)
+
+print(omega_max)
