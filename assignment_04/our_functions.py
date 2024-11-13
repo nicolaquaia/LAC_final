0d162c3aad44f815784e6e36c35221e1c8714e84
Bogusz <159239562+boguszglaza@users.noreply.github.com>
2024-11-13 17:12:37 +0100

    Updated with AEP and tower clearence

--- assignment_04/our_functions.py
+++ assignment_04/our_functions.py
@@ -1,13 +1,8 @@
-"""
-useful function for assignment 4 but can be expanded
-Created on Sat 9/11
-
-@author: Group 1
-"""
 
 import numpy as np
 from our_values import *
 from lacbox.io import load_stats
+from scipy.integrate import simpson
 
 def DEL_calculation(STATS_PATH, SUBFOLDER, chan_ids, CHAN_DESCS, wohler_4, wohler_10,
                     ws_prob,
@@ -47,7 +42,10 @@ def DEL_calculation(STATS_PATH, SUBFOLDER, chan_ids, CHAN_DESCS, wohler_4, wohle
             R_eq_array[idx] = (sum(HAWC2val[groups[idx]]**wohler_exp)/n_seed)**(1/wohler_exp) # takes an average load from the 6 cases
 
         # compute equivalent lifetime load
+        # R_eql = (trapezoid(ws_prob*R_eq_array**wohler_exp, ws_array) * n_t  / n_life )**(1/wohler_exp)      
         R_eql = (sum(ws_prob * R_eq_array**wohler_exp) * n_t / n_life) ** (1 / wohler_exp)
+        # very close to given result
+        # R_eql = (trapezoid(ws_prob*R_eq_array**wohler_exp, ws_array) * n_t  / n_seed/ n_life )**(1/wohler_exp)     # theoretically correct but not close to result
 
         data_channel = {
             'wohler_exp': wohler_exp,
@@ -67,6 +65,44 @@ def DEL_calculation(STATS_PATH, SUBFOLDER, chan_ids, CHAN_DESCS, wohler_4, wohle
 
     return data
 
+
+
+def filter(time, y, div):
+    n = len(time)
+    delta = int(div/2)
+
+    result = np.zeros(n)
+    for i in range(delta,n-delta):
+        result[i] = np.mean(y[i-delta:i+delta])
+    
+    for i in range(0,delta):
+        result[i] = np.mean(y[0:delta])
+    for i in range(n-delta, n):
+        result[i] = np.mean(y[n-delta:n])
+
+    return result
+
+def twr_clr_calculation(STATS_PATH, SUBFOLDER,CHAN_DESCS):
+    df, _ = load_stats(STATS_PATH, subfolder=SUBFOLDER, statstype='turb')
+    chan_id = 'Twrclr'
+    chan_df = df.filter_channel(chan_id, CHAN_DESCS)
+    h2_wind = chan_df['wsp']
+    HAWC2val = chan_df['min']
+    h2_wind, HAWC2val = np.array(h2_wind), np.array(HAWC2val)
+    i_h2 = np.argsort(h2_wind)
+    groups = [i_h2[i:i + 6] for i in range(0, len(i_h2), 6)]
+    ws_array = np.zeros(20)
+    twr_clr_array = np.zeros(20)
+    for idx, group in enumerate(groups):
+        ws_array[idx] = h2_wind[groups[idx]][0]
+        twr_clr_array[idx] = np.amin(HAWC2val[groups[idx]])
+
+    result = {
+        'ws' : ws_array,                 # wind speeds
+        'twr_clr': twr_clr_array,                   # Edges of wind-speed bins
+    }
+
+    return result
 
 def AEP_calculation(STATS_PATH, SUBFOLDER,CHAN_DESCS):
     df, _ = load_stats(STATS_PATH, subfolder=SUBFOLDER, statstype='turb')
@@ -101,22 +137,5 @@ def AEP_calculation(STATS_PATH, SUBFOLDER,CHAN_DESCS):
         'power' : power_array/1e6,      # Power in each bin [MW]
         'AEP' : AEP/1e9                 # annual energy production [GWh]
     }
-
-    return result
-
-
-
-def filter(time, y, div):
-    n = len(time)
-    delta = int(div/2)
-
-    result = np.zeros(n)
-    for i in range(delta,n-delta):
-        result[i] = np.mean(y[i-delta:i+delta])
-    
-    for i in range(0,delta):
-        result[i] = np.mean(y[0:delta])
-    for i in range(n-delta, n):
-        result[i] = np.mean(y[n-delta:n])
 
     return result
