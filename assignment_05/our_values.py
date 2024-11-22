2185911b4a9ea7f4b5690ea925d42b415c2b7568
Nicola Quaia <nicolaquaia@gmail.com>
2024-11-22 14:21:18 +0100

    initialization

---
+++ assignment_05/our_values.py
@@ -0,0 +1,22 @@
+#Our values
+"""
+valued computed during assignment 01 that are often used in the computations
+Created on Thu Oct 17 2024
+
+@author: Nicola
+"""
+
+R_X=89.16
+V_RATED_X = 11.4
+R_HUB = 2.8
+B = 3
+RHO = 1.225
+
+V_RATED_Y = 11.197563974599802
+R_Y = 91.59899634459074
+
+
+TSR_OPT = 7.2631578947368425
+OMEGA_MAX = 8.337868262998404           # [rpm]
+CP_MAX = 0.4414323017299089
+POWER_MAX = 10638.3E+03
